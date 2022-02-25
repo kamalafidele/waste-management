899fc483f7c18d4ba1a26f710c877a4cf6680eec
unknown <Kamalafizzet45@gmail.com>
2022-02-25 09:14:52 +0200

    added database configuration and company functionality

--- Backend/src/Application.java
+++ Backend/src/Application.java
@@ -3,16 +3,21 @@ import java.net.ServerSocket;
 
 public class Application {
     public static void main(String[] args){
-
+    // THIS THE ENTRY FOR OUR SERVER APPLICATION
         try{
-            var serverSocket=new ServerSocket(2500);
+            ServerSocket serverSocket=new ServerSocket(2500);
             System.out.println("########## SERVER RUNNING ON PORT 2500 ##############");
 
              while(3>2){
                  var socket=serverSocket.accept();
                  ThreadHandler threadHandler=new ThreadHandler(socket);
+
+                 // THIS IS TO CREATE A THREAD FOR EACH NEW CONNECTION TO OUR SERVER
                  threadHandler.start();
              }
-        }catch(IOException exception){}
+
+        }catch(IOException exception){
+            exception.printStackTrace();
+        }
     }
 }
--- Backend/src/Repositories/CompanyRepo.java
+++ Backend/src/Repositories/CompanyRepo.java
@@ -1,4 +1,26 @@
 package Repositories;
 
+import Config.DatabaseConnection;
+import Models.Company;
+
+import java.sql.ResultSet;
+
 public class CompanyRepo {
+      DatabaseConnection database;
+      public CompanyRepo(){
+          database=new DatabaseConnection();
+      }
+
+      public ResultSet findAll(){
+          return database.select("SELECT * FROM Company");
+      }
+
+      public ResultSet findById(int id){
+          return database.select("SELECT * FROM Company WHERE id = "+id);
+      }
+
+      public void save(Company company){
+          database.insert("INSERT INTO Company(name,email,paymentCode,password) VALUES ("+company.getName()+","+company.getEmail()+","+
+                           company.getPaymentCode()+","+company.getPassword()+")");
+      }
 }
