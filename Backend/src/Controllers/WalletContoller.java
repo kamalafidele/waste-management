59d864485cadde232e00c1f883dc9a3205e61da4
A. Belard <angebelard@Gmail.com>
2022-02-27 19:28:29 +0200

    Update WalletContoller.java

--- Backend/src/Controllers/WalletContoller.java
+++ Backend/src/Controllers/WalletContoller.java
@@ -1,5 +1,13 @@
 package Controllers;
 
+import Repositories.WalletRepo;
+import org.codehaus.jackson.map.ObjectMapper;
+
+import java.io.DataOutputStream;
+
 public class WalletContoller {
+    private static DataOutputStream toClient;
+    private static WalletRepo walletRepo;
+    private ObjectMapper mapper;
     
 }
