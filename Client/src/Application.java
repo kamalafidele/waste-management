6223c3c1dc9c79703df447e1013119a5241a220a
Saad <byiringirosaad@gmail.com>
2022-02-27 18:18:25 +0200

    project starting

--- Client/src/Application.java
+++ Client/src/Application.java
@@ -17,14 +17,44 @@ public class Application {
             DataInputStream  fromServer=new DataInputStream(socket.getInputStream());
 
 
-            System.out.println("");
-            System.out.println(BLUE + "\t\t\t    \\\\              //\\\\             //   ==================   ||\\\\             //||   ================== ");
-            System.out.println(BLUE + "\t\t\t     \\\\            //  \\\\           //    ||                   || \\\\           // ||   ||");
-            System.out.println(BLUE + "\t\t\t      \\\\          //    \\\\         //     ||                   ||  \\\\         //  ||   ||");
-            System.out.println(BLUE + "\t\t\t       \\\\        //      \\\\       //      ==================   ||   \\\\       //   ||   ================== ");
-            System.out.println(BLUE + "\t\t\t        \\\\      //        \\\\     //                       ||   ||    \\\\     //    ||                   || ");
-            System.out.println(BLUE + "\t\t\t         \\\\    //          \\\\   //                        ||   ||     \\\\   //     ||                   ||");
-            System.out.println(BLUE + "\t\t\t          \\\\  //            \\\\ //         ==================   ||      \\\\ //      ||   ================== ");
+            System.out.println("--------------------------------------------------WELCOME TO----------------------------------------------" + RESET);
+            System.out.println(BLUE + "\\\\              //\\\\             //   ==================   ||\\\\             //||   ================== " + RESET);
+            System.out.println(BLUE + " \\\\            //  \\\\           //    ||                   || \\\\           // ||   ||" + RESET);
+            System.out.println(BLUE + "  \\\\          //    \\\\         //     ||                   ||  \\\\         //  ||   ||" + RESET);
+            System.out.println(BLUE + "   \\\\        //      \\\\       //      ==================   ||   \\\\       //   ||   ================== " + RESET);
+            System.out.println(BLUE + "    \\\\      //        \\\\     //                       ||   ||    \\\\     //    ||                   || " + RESET);
+            System.out.println(BLUE + "     \\\\    //          \\\\   //                        ||   ||     \\\\   //     ||                   ||" + RESET);
+            System.out.println(BLUE + "      \\\\  //            \\\\ //         ==================   ||      \\\\ //      ||   ================== " + RESET);
+            System.out.println("\n\n");
+
+            System.out.println("--------Please choose your role----------");
+            System.out.println("1.Admin");
+            System.out.println("2.District");
+            System.out.println("3.Company");
+            System.out.println("4.Confirmer");
+            System.out.println("5.House");
+            System.out.print("Your choice: ");
+            int choice = keyboard.nextInt();
+
+            switch (choice){
+                case 1:
+                    System.out.println("You are an admin");
+                    break;
+                case 2:
+                    System.out.println("You are a district!");
+                    break;
+                case 3:
+                    System.out.println("You are a company!");
+                    break;
+                case 4:
+                    System.out.println("You are a confirmer!");
+                    break;
+                case 5:
+                    System.out.println("You are a house!");
+                    break;
+                default:
+                    break;
+            }
 
             String response=fromServer.readUTF();
             System.out.println(response);
