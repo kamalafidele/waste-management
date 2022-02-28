888914066705c93f88413fad6fbe2b67dd3a03ea
A. Belard <angebelard@Gmail.com>
2022-02-28 07:39:52 +0200

    Update WalletContoller.java

--- Backend/src/Controllers/WalletContoller.java
+++ Backend/src/Controllers/WalletContoller.java
@@ -1,6 +1,6 @@
 package Controllers;
 
-import Repositories.WalletRepo;
+import Repositories.WalletsRepoHandler;
 import org.codehaus.jackson.map.ObjectMapper;
 
 import java.io.DataOutputStream;
@@ -8,17 +8,19 @@ import java.sql.ResultSet;
 
 public class WalletContoller {
     private DataOutputStream toClient;
-    private static WalletRepo walletRepo;
+    private static WalletsRepoHandler walletRepo;
     private ObjectMapper mapper;
 
     public WalletContoller(){
-        walletRepo = new WalletRepo();
+        walletRepo = new WalletsRepoHandler();
         mapper = new ObjectMapper();
     }
 
-    public void getWallet(String request, DataOutputStream toClient){
+    public void getUserWallet(String request, DataOutputStream toClient){
         this.toClient = toClient;
         int userId = Integer.parseInt(request);
-        ResultSet walletResult = walletRepo.findWalletById(userId);
+        ResultSet walletResult = walletRepo.findWalletByUserId(userId);
     }
+
+    
 }
