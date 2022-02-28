bb9a8005574b18b1552f95b946a57ca741a8fd92
Mfitumukiza Peter <mfitep6@gmail.com>
2022-02-28 06:05:34 +0200

    company analytics

--- Backend/src/Repositories/AnalyticsRepo.java
+++ Backend/src/Repositories/AnalyticsRepo.java
@@ -1,6 +1,21 @@
 package Repositories;
 
+import Config.DatabaseConnection;
 public class AnalyticsRepo {
+    DatabaseConnection database;
+    public AnalyticsRepo(){
+        database = new DatabaseConnection();
+    }
 
+    public String districtAnalytics(String period){
+//        select analytics for district in that period
+        return "";
+    }
 
+    public String companyAnalytics(String period){
+        String analytics = "Number of service provided by us:  2\nTotal amount of money so far : 20000frw\n" +
+                "Percentage at which service is provided: 80%\n========END========";
+//        select analytics for company in the specified period
+        return analytics;
+    }
 }
