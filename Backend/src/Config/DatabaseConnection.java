2aa78aba3c666d283bb73e6792e80e52ebc828a9
Mizero-eloi <80544579+Mizero-eloi@users.noreply.github.com>
2022-02-28 22:39:41 +0200

    my first commits

--- Backend/src/Config/DatabaseConnection.java
+++ Backend/src/Config/DatabaseConnection.java
@@ -4,9 +4,9 @@ import java.sql.*;
 
 public class DatabaseConnection {
     String driver="com.mysql.jdbc.Driver";
-    String url="jdbc:mysql://localhost:3306/waste_management?characterEncoding=latin1";
+    String url="jdbc:mysql://localhost:3306/wasteManagementDatabase?characterEncoding=latin1";
     String username="root";
-    String password="Saad@123";
+    String password="ae789789";
     Connection connection=null;
     Statement statement=null;
     ResultSet data=null;
