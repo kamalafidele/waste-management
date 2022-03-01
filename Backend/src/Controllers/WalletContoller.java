0b9aeb9d2c4e075d0d8fae4d7297f3dd00a0a9c4
A. Belard <angebelard@Gmail.com>
2022-03-01 11:44:29 +0200

    Update WalletContoller.java

--- Backend/src/Controllers/WalletContoller.java
+++ Backend/src/Controllers/WalletContoller.java
@@ -4,12 +4,42 @@ import Repositories.WalletsRepoHandler;
 import org.codehaus.jackson.map.ObjectMapper;
 
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.sql.ResultSet;
 
 public class WalletContoller {
     private DataOutputStream toClient;
     private static WalletsRepoHandler walletRepo;
     private ObjectMapper mapper;
+
+    public void whichWallet( String request, DataOutputStream toClient ) {
+        this.toClient = toClient;
+        switch (request.split("/")[1]) {
+            case "admin":
+                getAdminWallet(request, toClient);
+                break;
+            case "company":
+                getCompanyWallet(request, toClient);
+                break;
+            case "district":
+                getDistrictWallet(request, toClient);
+                break;
+            case "client":
+                getUserWallet(request, toClient);
+                break;
+            default:
+                returnWallet("Please specify the wallet owner type!");
+                break;
+        }
+    }
+
+    public void returnWallet( String response ) {
+        try {
+            toClient.writeUTF(response);
+        } catch ( IOException exception ) {
+            exception.printStackTrace();
+        }
+    }
 
     public WalletContoller(){
         walletRepo = new WalletsRepoHandler();
