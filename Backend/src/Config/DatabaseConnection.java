3111c244f965bbcb3401d9ac6f0f40f84ed7b02f
Saad <byiringirosaad@gmail.com>
2022-03-02 13:02:18 +0200

    admin dashboard

--- Backend/src/Config/DatabaseConnection.java
+++ Backend/src/Config/DatabaseConnection.java
@@ -4,7 +4,7 @@ import java.sql.*;
 
 public class DatabaseConnection {
     String driver="com.mysql.jdbc.Driver";
-    String url="jdbc:mysql://localhost:3306/year2?characterEncoding=latin1";
+    String url="jdbc:mysql://localhost:3306/waste_management?characterEncoding=latin1";
     String username="root";
     String password="Saad@123";
     Connection connection=null;
