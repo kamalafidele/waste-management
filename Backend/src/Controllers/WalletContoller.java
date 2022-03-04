fd2076c195b6d3bb7d7e46b74abd63dc5ad9e5ae
A. Belard <angebelard@Gmail.com>
2022-03-04 08:39:51 +0200

    Update WalletContoller.java

--- Backend/src/Controllers/WalletContoller.java
+++ Backend/src/Controllers/WalletContoller.java
@@ -92,6 +92,6 @@ public class WalletContoller {
         this.toClient = toClient;
         int companyId = Integer.parseInt(request);
         ResultSet walletResult = walletRepo.findWalletByCompanyId(companyId);
-        
+        getWalletBalance(walletResult);
     }
 }
