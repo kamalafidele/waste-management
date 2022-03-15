3997d6771be8a08384560e1004407d640b5fcd76
Teta Lena <tetalenaa@gmail.com>
2022-03-15 14:12:56 +0200

    changes before pull tuesday

--- Backend/src/Config/DatabaseConnection.java
+++ Backend/src/Config/DatabaseConnection.java
@@ -4,7 +4,7 @@ import java.sql.*;
 
 public class DatabaseConnection {
     String driver="com.mysql.jdbc.Driver";
-    String url="jdbc:mysql://localhost:3306/waste_management?characterEncoding=latin1";
+    String url="jdbc:mysql://localhost:3306/waste_management";
     String username="root";
     String password="teta2005";
     Connection connection=null;
