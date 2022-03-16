1c23400f72eebff00807aa0c30107c8171dd8936
Mizero-eloi <80544579+Mizero-eloi@users.noreply.github.com>
2022-03-16 09:39:15 +0200

    changes

--- Backend/src/Config/DatabaseConnection.java
+++ Backend/src/Config/DatabaseConnection.java
@@ -6,7 +6,7 @@ public class DatabaseConnection {
     String driver="com.mysql.jdbc.Driver";
     String url="jdbc:mysql://localhost:3306/wasmanagement?characterEncoding=latin1";
     String username="root";
-    String password="marv1nk@rs";
+    String password="ae789789";
     Connection connection=null;
     Statement statement=null;
     ResultSet data=null;
