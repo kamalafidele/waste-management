5708f8dc80db16d4df2a1a0f114ddc5826addd06
spfvtnjr <turinumugishasouvenir@gmail.com>
2022-03-18 08:04:17 -0700

    Login changes

--- Client/src/Components/Wallet.java
+++ Client/src/Components/Wallet.java
@@ -10,18 +10,14 @@ import java.io.IOException;
 public class Wallet {
     DataOutputStream outToServer;
     DataInputStream fromServer;
-
     public Wallet(DataOutputStream toServer, DataInputStream fromServer) {
         this.outToServer=toServer;
         this.fromServer=fromServer;
     }
-
-
     public void showWallet(int userId){
         String request="wallet/" + userId;
         try {
             outToServer.writeUTF(request);
-            System.out.println("SENT REQUEST");
             String response=fromServer.readUTF();
             System.out.printf("Your wallet balance is: " + response +" Rwf");
         }
