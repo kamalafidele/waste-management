a7dfe7b1fe3cae6242f861b3ef37746fdd11a147
giseleMwizera <mamengisele@gmail.com>
2022-05-19 21:41:11 +0200

    initialised Company GUI

--- Client/src/Desktop/Components/Company.java
+++ Client/src/Desktop/Components/Company.java
@@ -1,4 +1,17 @@
 package Desktop.Components;
 
-public class Company {
+import javax.swing.*;
+
+public class Company{
+ Company(){
+    JFrame frame = new JFrame();
+
+ }
+    public static void main(String[] args){
+        new Company();
+    }
+    public void viewCompanyLocations(){
+
+    }
 }
+
