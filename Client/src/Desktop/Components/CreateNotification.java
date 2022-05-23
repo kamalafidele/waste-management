3e03cda92eb2906617ce73c148eb4925b48e8fcf
Fiat Bruno <fiatbruno10@gmail.com>
2022-05-23 06:35:22 +0200

    Added placeholders

--- Client/src/Desktop/Components/CreateNotification.java
+++ Client/src/Desktop/Components/CreateNotification.java
@@ -1,66 +1,106 @@
 package Desktop.Components;
 
+import Desktop.EventHandlers.PlaceHolderHandler;
 import com.github.lgooddatepicker.components.DatePicker;
 import com.github.lgooddatepicker.components.DatePickerSettings;
 import net.miginfocom.swing.MigLayout;
+import org.codehaus.jackson.map.ObjectMapper;
 
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.time.DayOfWeek;
 
+/*
+ * @authors: Fiat Bruno, Ineza Naillah
+ *
+ */
+
 public class CreateNotification extends JFrame {
+    public static void main(String[] args) {
+        new CreateNotification();
+    }
+    ObjectMapper mapper = new ObjectMapper();
+
+    DataOutputStream toServer;
+    DataInputStream fromServer;
+    Color dodgerBlue = new Color(52,143,235);
+    JPanel mainPanel;
+    JPanel insidePanel;
+    JLabel notification;
+    JLabel description;
+    JLabel notificationName;
+    JLabel processName;
+    JLabel status;
+    JLabel assignedGroup;
+    JLabel renotify;
+    JLabel date;
+    JLabel content;
+    JLabel message;
+    JTextField notificationTextField;
+    JTextField processTextField;
+    JComboBox statusChoice;
+    JComboBox groupChoice;
+    JComboBox periodChoice;
+    JTextArea textArea;
+    JPanel buttonPanel;
+    JButton saveButton;
+    JButton closeButton;
     public CreateNotification(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("Create Notifications");
         this.setSize(800, 800);
         this.setResizable(false);
 
-        JPanel mainPanel = new JPanel(new MigLayout("center, wrap, fill, insets 20", "20:push[]20:push", "20:push[]20[]20[]20:push"));
-        JLabel notification = new JLabel("Create Notifications");
-        notification.setFont(new Font(null, Font.BOLD, 20));
-        notification.setForeground(new Color(0, 66, 101));
-        JPanel insidePanel = new JPanel(new MigLayout("wrap", "[]10:push[]10:push[]10:push[]", "[]10[]"));
+        mainPanel = new JPanel(new MigLayout("center, wrap, fill, insets 20", "20:push[]20:push", "20:push[]20[]20[]20:push"));
+        notification = new JLabel("Create Notifications");
+        notification.setFont(new Font("Inter", Font.BOLD, 20));
+        notification.setForeground(dodgerBlue);
+        insidePanel = new JPanel(new MigLayout("wrap", "[]10:push[]10:push[]10:push[]", "[]10[]"));
 
-        JLabel description = new JLabel("Description");
-        description.setFont(new Font(null, Font.BOLD, 15));
-        description.setForeground(new Color(0, 66, 101));
-        JLabel notificationName = new JLabel("Notification Name");
-        JTextField notificationTextField = new JTextField();
-        JLabel processName = new JLabel("Process Name");
-        JTextField processTextField = new JTextField();
-        JLabel status = new JLabel("Status");
+        description = new JLabel("Description");
+        description.setFont(new Font("Inter", Font.BOLD, 15));
+        description.setForeground(dodgerBlue);
+        notificationName = new JLabel("Notification Name");
+        notificationTextField = new JTextField();
+        notificationTextField.addFocusListener(new PlaceHolderHandler(notificationTextField, "Name your notification"));
+        processName = new JLabel("Process Name");
+        processTextField = new JTextField();
+        processTextField.addFocusListener(new PlaceHolderHandler(processTextField, "Name your process"));
+        status = new JLabel("Status");
         String[] statusChoices = {
                 "Active",
                 "Inactive"
         };
-        JComboBox statusChoice = new JComboBox(statusChoices);
-        JLabel assignedGroup = new JLabel("Assigned Group");
+        statusChoice = new JComboBox(statusChoices);
+        assignedGroup = new JLabel("Assigned Group");
         String[] groupChoices = {
                 "Citizens",
                 "Companies",
                 "Districts"
         };
-        JComboBox groupChoice = new JComboBox(groupChoices);
-        JLabel date = new JLabel("Notify On");
+        groupChoice = new JComboBox(groupChoices);
+        date = new JLabel("Notify On");
 
         DatePickerSettings dateSettings = new DatePickerSettings();
         dateSettings.setFirstDayOfWeek(DayOfWeek.MONDAY);
         DatePicker datePicker = new DatePicker(dateSettings);
 
-        JLabel renotify = new JLabel("Renotify");
+        renotify = new JLabel("Renotify");
         String[] periodChoices = {
                 "Every Week",
                 "Every Month",
                 "Every Year",
                 "Every Five Years"
         };
-        JComboBox periodChoice = new JComboBox(periodChoices);
-        JLabel content = new JLabel("Content");
-        content.setFont(new Font(null, Font.BOLD, 15));
-        content.setForeground(new Color(0, 66, 101));
-        JLabel message = new JLabel("Message");
-        JTextArea textArea = new JTextArea("Write your message here");
+        periodChoice = new JComboBox(periodChoices);
+        content = new JLabel("Content");
+        content.setFont(new Font("Inter", Font.BOLD, 15));
+        content.setForeground(dodgerBlue);
+        message = new JLabel("Message");
+        textArea = new JTextArea("Write your message here");
 
         insidePanel.add(description, "span");
         insidePanel.add(notificationName);
@@ -79,9 +119,9 @@ public class CreateNotification extends JFrame {
         insidePanel.add(message);
         insidePanel.add(textArea, "span, grow, height 200");
 
-        JPanel buttonPanel = new JPanel();
-        JButton saveButton = new JButton("Save");
-        JButton closeButton = new JButton("Close");
+        buttonPanel = new JPanel();
+        saveButton = new JButton("Save");
+        closeButton = new JButton("Close");
 
         buttonPanel.add(saveButton);
         buttonPanel.add(closeButton);
