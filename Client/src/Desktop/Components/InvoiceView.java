779e27000d11ef7d2c11cba36e166f19aa36d37f
a-belard <angebelard@gmail.com>
2022-05-27 09:25:56 +0200

    Update InvoiceView.java

--- Client/src/Desktop/Components/InvoiceView.java
+++ Client/src/Desktop/Components/InvoiceView.java
@@ -8,29 +8,31 @@ import java.awt.print.PrinterException;
 public class InvoiceView extends JFrame {
     public JTextArea textArea;
     public JButton download_pdf;
+
     Color dodgerBlue = new Color(52,143,235);
 
-    public InvoiceView(Object [] data){
+    public InvoiceView(Object [] data, JTable table){
+
         Object inv_date = data[4];
         Object usr_name = data[1];
         Object service = data[2];
         Object price = data[3];
-        setBounds(200, 200, 500, 600);
+        setBounds(200, 50, 1000, 1000);
         setLayout(null);
         setVisible(true);
 
         JPanel panel1 = new JPanel();
-        panel1.setBounds(20,170,460,450);
         panel1.setBorder(BorderFactory.createBevelBorder(1));
         panel1.setLayout(null);
         add(panel1);
 
         panel1.setVisible(true);
-        panel1.setBounds(300,100,1020,740);
-        getContentPane().setBackground(Color.LIGHT_GRAY);
+        panel1.setBounds(300,100,600,840);
+        getContentPane().setBackground(Color.white);
 
         textArea = new JTextArea();
-        textArea.setBounds(590,170,360,450);
+        textArea.setBounds(70,60,360,450);
+        textArea.setVisible(true);
         textArea.setFont(new Font("Charcoal CY",Font.PLAIN,18));
         textArea.setBorder(BorderFactory.createBevelBorder(1));
         panel1.add(textArea);
@@ -51,13 +53,15 @@ public class InvoiceView extends JFrame {
                 "\n=============================================");
 
         download_pdf = new JButton("Print Bill");
-        download_pdf.setBounds(750,640,200,50);
+        download_pdf.setBounds(150,520,200,50);
         download_pdf.setFocusable(false);
         download_pdf.setFont(new Font("Mongolian Baiti",Font.BOLD,16));
         download_pdf.setBackground(dodgerBlue);
         download_pdf.setForeground(Color.WHITE);
         download_pdf.addActionListener(this::download_pdf_Button_Action);
         panel1.add(download_pdf);
+
+
     }
 
     private void download_pdf_Button_Action(ActionEvent evt)
@@ -69,8 +73,9 @@ public class InvoiceView extends JFrame {
         }
     }
 
-    public static void main(String[] args) {
-        Object [] data = {"0", "Colombe", "Security", "5000", "24-05-2022"};
-        new InvoiceView(data);
-    }
+//    public static void main(String[] args) {
+//      Object [] data = {"0","Colombe","Security","5000","24-05-2022"};
+//          new InvoiceView(data);
+//
+//    }
 }
